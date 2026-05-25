4adc22e775fc194325adb700ad45be186f6493de
ishikajais27 <ishikajais09876@gmail.com>
2026-05-25 17:11:39 +0530

    understand filter and lambda

---
+++ 45q.py
@@ -0,0 +1,6 @@
+#A lambda function = small anonymous function (without name) like arrow function in js   lambda arguments: expression
+num = [1,2,3,4,5,6,7,8,9,10]
+
+even = list(filter(lambda x: x % 2 == 0, num))
+
+print(even)
