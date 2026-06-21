c209d9bcef90b24164700d351f5d18bce00645ce
ishikajais27 <ishikajais09876@gmail.com>
2026-06-21 20:18:04 +0530

    understand encode and utf-8

---
+++ 61q.py
@@ -0,0 +1,9 @@
+s = input("Enter string: ")
+
+u = s.encode("utf-8")
+
+print(u)
+
+#Convert a string into bytes using a specific encoding. 
+# utf-8 - Unicode Transformation Format - 8 bit. A very popular encoding method. 
+# In Python 3 unicode() does not exist
