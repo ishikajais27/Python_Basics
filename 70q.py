60cb7197e38b11330bc67f86571068c767d05d2d
ishikajais27 <ishikajais09876@gmail.com>
2026-06-22 14:39:22 +0530

    understand random() method

---
+++ 70q.py
@@ -0,0 +1,6 @@
+import random
+
+num = random.random() * 90 + 10
+print(num)
+
+#random.random() gives a float between 0 and 1.Multiplying by 90 gives a float between 0 and 90. Adding 10 shifts the range to 10 and 100
