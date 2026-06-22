8d91337ba7ea22cf8b902c2c3339c41815865bbf
ishikajais27 <ishikajais09876@gmail.com>
2026-06-22 14:50:01 +0530

    understand zlib.compress() and zlib.decompress() methods

---
+++ 76q.py
@@ -0,0 +1,13 @@
+import zlib
+
+text = "hello world!hello world!hello world!hello world!"
+
+compressed = zlib.compress(text.encode())
+
+decompressed = zlib.decompress(compressed).decode()
+
+print("Compressed:", compressed)
+print("Decompressed:", decompressed)
+
+#encode() converts the string to bytes. zlib.compress() compresses the bytes. zlib.decompress() decompresses the bytes.
+# decode() converts the bytes back to a string.zlib.compress() works only with bytes-like objects, not Python strings.
